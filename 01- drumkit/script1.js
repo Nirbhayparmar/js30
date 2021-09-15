644c81fd330bd3c00b25d91de26f0ec9927a6db5
Nirbhay <niravnirbhay94@gmail.com>
2021-09-16 01:34:10 +0530

    javascript-30 projects

---
+++ 01- drumkit/script1.js
@@ -0,0 +1,15 @@
+function playAudio(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+window.addEventListener("keydown", playAudio);
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
---
+++ 02- js and css clock/whatToDo.md
@@ -0,0 +1,3 @@
+-[x] - set transform property
+
+-[x] - write the css propety for transition and do the same for all minute and hour hand.
